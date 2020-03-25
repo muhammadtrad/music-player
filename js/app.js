96844f0b7e120a21d18e6ba30396300f273a067e
xmagnetisx <muhammad.trad@yahoo.com>
2020-03-24 21:37:27 -0700

    added multiple new modules

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
 import PlayList from './modules/playlist.js';
+import PlayInfo from './modules/play-info.js';
+import TrackBar from './modules/track-bar.js';
 
 PlayList.init();
+PlayInfo.init();
--- js/modules/playlist.js
+++ js/modules/playlist.js
@@ -1,19 +1,89 @@
 import { songsList } from '../data/songs.js';
+import PlayInfo from './play-info.js';
+import TrackBar from './track-bar.js';
 
 const Playlist = (_ => {
     //data or state
     let songs = songsList;
     let currentlyPlayingIndex = 0;
     let currentSong = new Audio(songs[currentlyPlayingIndex].url);
-    let isPlaying = false;
 
     //cache the DOM
     const playlistEl = document.querySelector(".playlist");
 
     const init = _ =>{
         render();
+        listeners();
+
+        PlayInfo.setState({
+          songsLength: songs.length,
+          isPlaying: !currentSong.paused
+        })
     }
 
+    const flip = _ => {
+      togglePlayPause();
+      render();
+    }
+
+    const changeAudioSrc = _ => {
+      currentSong.src = songs[currentlyPlayingIndex].url;
+    }
+
+    const togglePlayPause = _ => {
+      return currentSong.paused ? currentSong.play() : currentSong.pause();
+    }
+
+
+    const mainPlay = clickedIndex => {
+      if (currentlyPlayingIndex === clickedIndex){
+        //toggle play or pause
+        togglePlayPause();
+      } else{
+        currentlyPlayingIndex = clickedIndex;
+        changeAudioSrc();
+        togglePlayPause();
+      }
+      
+        PlayInfo.setState({
+          songsLength: songs.length,
+          isPlaying: !currentSong.paused
+        });
+
+
+    }
+
+    const playNext = _ =>{
+      if (songs[currentlyPlayingIndex + 1]){
+      currentlyPlayingIndex++;
+      changeAudioSrc();
+      togglePlayPause();
+      render();
+      }
+    }
+    const listeners = _ =>{
+      //1. get the index of the li tag
+      //2. change the currentplayingindex to index of the li tag
+      //3. play or pause
+      //4. if it's not the same song, then change the src to that new song
+      playlistEl.addEventListener("click", (event) => {
+        if (event.target && event.target.matches(".fa")){
+          const listElem = event.target.parentNode.parentNode;
+          const listElemIndex = [...listElem.parentElement.children].indexOf(listElem);
+          mainPlay(listElemIndex);
+          render();
+        }
+      })
+
+      currentSong.addEventListener("timeupdate", _ =>{
+        TrackBar.setState(currentSong);
+        console.log(currentSong.currentTime);
+      })
+
+      currentSong.addEventListener("ended", _ => {
+        playNext();
+      })
+    }
     const render = _ => {
         let markup = '';
 
@@ -48,7 +118,8 @@ const Playlist = (_ => {
         playlistEl.innerHTML = markup;
     }
     return {
-       init
+       init,
+       flip
     }
 })();
 
